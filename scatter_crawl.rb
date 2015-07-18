24c7ad840b464ed00d9a9f3f0016c50658f32fb1
Gregory Kan <kan.gregory@gmail.com>
2015-07-18 12:12:19 +1200

    added scatter_crawl which picks a random line from every poem it comes across

--- scatter_crawl.rb
+++ scatter_crawl.rb
@@ -5,18 +5,22 @@ require 'anemone'
 url = 'http://www.poetryfoundation.org/'
 
 Anemone.crawl(url) do |anemone|
-  count = 0
   anemone.focus_crawl do |page|
     page.links.select {|link| link.to_s.include?('poetryfoundation')}
   end
   anemone.on_every_page do |page|
     if page.doc && page.doc.css('#poem div.poem') && page.doc.css('#poem div.poem').css('div') && !page.url.to_s.include?('poetrymagazine')
+      count = 0
       poem = page.doc.css('#poem div.poem').css('div')
+      limit = poem.length
+      random = Random.new.rand(0..limit)
       poem.each do |line|
-        if line.text.length < 100 && (line.text.include? 'snow')
+        if line.text.length < 100 && count == random
           puts line.text
         end
+        count +=1
       end
     end
   end
 end
+
